8d7b0cfa1ef254533e267644b38dd3e596e1a898
sukka <catisouka@gmail.com>
2023-11-08 14:54:21 +0100

    creating classes Graph, Chemin, Sommet, Arret and Chajara

---
+++ Arret.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Arret{
+        List<Sommet> sommets;
+        int valeur;
+        public Arret( List<Sommet> Sommets, int Valeur){
+            sommets = Sommets;
+            valeur = Valeur;
+        }
+    }
---
+++ Graph.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Graph
+    {
+        List<Sommet> Ens_Sommets;
+        List<Arret> Ens_arret;
+
+        public Graph(List<Sommet> s, List<Arret> a){
+            Ens_Sommets = s;
+            Ens_arret = a;
+        }
+    }
+    
