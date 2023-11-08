69a85980903ebe7f6a69ae582bfd385ffd0e2c87
sukka <catisouka@gmail.com>
2023-11-08 16:03:43 +0100

    Chemin.java

--- Chemin.java
+++ Chemin.java
@@ -8,12 +8,49 @@ public class Chemin{
     {
 
     }
-    public Chemin(List <Arret> A, List <Sommet> S, int V)
+    public Chemin(List <Arret> A)
     {
         this.Arrets=A;
-        this.Sommets=S;
-        this.Value=V;
+        for(int i=0;i<Arrets.size();i++)
+        {
+            if(i==0)
+            {
+                this.Sommets.add(this.Arrets.get(0).sommets.get(0));
+                this.Sommets.add(this.Arrets.get(0).sommets.get(1));
+            }
 
+            this.Sommets.add(this.Arrets.get(i).sommets.get(1));
+        }
+       
+    }
+    //fonction isCycle()
+    boolean isCycle()
+    {
+        boolean result;
+        
+        
+        for(int i=0;i<Sommets.size();i++)
+        {
+            String temp;
+            temp=Sommets.get(0).nom;
+            for(int j=0;j<Sommets.size();j++)
+            {
+                if(t[i]==t[j])
+                {
+                    j++;
+                }
+                if(temp == Sommets.get(i).nom)
+                {
+
+                }
+
+            }
+            
+          
+
+        }
+
+        return result;
     }
 
 
