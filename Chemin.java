168fea52b63d9b9e4682171bdd5dcdde2f4b6f5d
sukka <catisouka@gmail.com>
2023-11-08 15:28:05 +0100

    defining chemin

--- Chemin.java
+++ Chemin.java
@@ -1,5 +1,22 @@
 import java.util.List;
 
 public class Chemin{
+    public List <Arret> Arrets;
+    public List <Sommet> Sommets;
+    public int Value;
+    public Chemin()
+    {
+
+    }
+    public Chemin(List <Arret> A, List <Sommet> S, int V)
+    {
+        this.Arrets=A;
+        this.Sommets=S;
+        this.Value=V;
+
+    }
+
+
+
         
     }
